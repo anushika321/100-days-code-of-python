21ed732b81658ec8bdd41addddb7b2f15a5d2c69
anushika321 <anushikachoudhary392@gmail.com>
2025-09-25 14:27:07 +0530

    Add files via upload

---
+++ q5.py
@@ -0,0 +1,10 @@
+a = 50 
+b = 3
+
+
+print("the value of ", a , "*" , 3 ,"is: ", a*b)
+print("the value of ", a , "/" , 3 ,"is: ", a/b)
+print("the value of ", a , "-" , 3 ,"is: ", a-b)
+print("the value of ", a ,  "+"  , 3 ,"is: ", a+b)
+
+
